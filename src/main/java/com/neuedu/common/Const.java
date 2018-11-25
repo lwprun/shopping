6fd293363acf3d578d0522e37c9882bf1ea12d37
lwprun <1027148349@qq.com>
2018-11-25 22:08:00 +0800

    tomcat集群

--- src/main/java/com/neuedu/common/Const.java
+++ src/main/java/com/neuedu/common/Const.java
@@ -5,36 +5,36 @@ public class Const {
     public static final String CURRENTUSER="current_user";
     public static final String AUTOLOGINCOOKIE = "autologincookie";
     public static final String TRADE_SUCCESS="TRADE_SUCCESS";
-    public  enum  ReponseCodeEnum{
-
-        NEED_LOGIN(2,"需要登录"),
-        NO_PRIVILEGE(3,"无权限操作"),
-        ;
-
-
-        private  int  code;
-        private String desc;
-        private ReponseCodeEnum(int code,String desc){
-            this.code=code;
-            this.desc=desc;
-        }
-
-        public int getCode() {
-            return code;
-        }
-
-        public void setCode(int code) {
-            this.code = code;
-        }
-
-        public String getDesc() {
-            return desc;
-        }
-
-        public void setDesc(String desc) {
-            this.desc = desc;
-        }
-    }
+//    public  enum  ReponseCodeEnum{
+//
+//        NEED_LOGIN(2,"需要登录"),
+//        NO_PRIVILEGE(3,"无权限操作"),
+//        ;
+//
+//
+//        private  int  code;
+//        private String desc;
+//        private ReponseCodeEnum(int code,String desc){
+//            this.code=code;
+//            this.desc=desc;
+//        }
+//
+//        public int getCode() {
+//            return code;
+//        }
+//
+//        public void setCode(int code) {
+//            this.code = code;
+//        }
+//
+//        public String getDesc() {
+//            return desc;
+//        }
+//
+//        public void setDesc(String desc) {
+//            this.desc = desc;
+//        }
+//    }
     public enum RoleEnum{
         ROLE_ADMIN(0,"管理员"),
         ROLE_CUSTOMER(1,"普通用户");
